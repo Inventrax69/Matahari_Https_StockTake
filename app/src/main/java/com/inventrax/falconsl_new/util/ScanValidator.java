8d14e37f0190136aa07ede0a8801c76193139974
Padmaja.B <padmaja.bodsakurthi@inventrax.com>
2021-03-02 12:14:18 +0530

    Initial commit

---
+++ app/src/main/java/com/inventrax/falconsl_new/util/ScanValidator.java
@@ -0,0 +1,73 @@
+package com.inventrax.falconsl_new.util;
+
+/**
+ * Created by karthik.m on 05/11/2018.
+ */
+
+public class ScanValidator {
+
+    public static boolean IsItemScanned(String scannedData) {
+        if (scannedData.split("[-]").length == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // Scan Validator
+
+    public static boolean isItemScanned(String scannedData) {
+        if (scannedData.split("[|]").length == 9 || scannedData.split("[|]").length == 5 || scannedData.split("[|]", -1).length == 9) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isContainerScanned(String scanneddata) {
+        if (scanneddata.startsWith("202")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean isLocationScanned(String scannedData) {
+        if ((scannedData.length() == 7 || scannedData.length() == 9) && isNumeric(scannedData.substring(5, 7))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean isDockLocationScanned(String scannedData) {
+        if (scannedData.length() == 9 && scannedData.startsWith("DZ")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean isNumeric(String ValueToCheck) {
+
+        try {
+            Double result = Double.parseDouble(ValueToCheck);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public static boolean isBinScanned(String scannedData) {
+        if (scannedData.split("[-]").length == 5 && !isNumeric(scannedData.split("[-]")[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
